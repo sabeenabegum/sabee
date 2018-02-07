4683e52fd7bcc460176f3c760bc3c5cd6cc73ab5
sabeenabegum <31505413+sabeenabegum@users.noreply.github.com>
2018-02-07 10:22:48 +0000

    Create pro27.c

---
+++ pro27.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	char ch;
+	scanf("%d",&ch);
+           if(ch>=0&&ch<=9)
+		{
+			printf("yes");
+		}
+			else
+			{
+				printf("no");
+			}
+		return 0;
+	}
+
