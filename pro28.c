d2c7e4d149a1c791c5e1db1199c3d62f4b6856ca
sabeenabegum <31505413+sabeenabegum@users.noreply.github.com>
2018-02-07 10:53:19 +0000

    Create pro28.c

---
+++ pro28.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int n,i,a[i];
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("%d %d",a[i],i);
+	}
+	return 0;
+}
