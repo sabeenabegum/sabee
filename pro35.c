d585a58fd32b8044bda7bbb51e5dcdd56b336d1c
sabeenabegum <31505413+sabeenabegum@users.noreply.github.com>
2018-02-19 15:50:46 +0530

    Create pro35.c

---
+++ pro35.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void) 
+{
+	int i,c=0;
+	char a[30];
+	gets(a);
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i]>='0'&&a[i]<='9')
+		{
+			c++;
+		}
+	}
+	printf("%d",c);
+	return 0;
+}
