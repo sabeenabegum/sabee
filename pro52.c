7eafe47962466906e86bcaa77f379e67511b7ffd
sabeenabegum <31505413+sabeenabegum@users.noreply.github.com>
2018-02-28 15:58:52 +0530

    Create pro52.c

---
+++ pro52.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+	int ch;
+	scanf("%d",&ch);
+	switch(ch)
+	{
+		case 1:
+		{
+			printf("one");
+		break;
+		}
+		case 2:
+		{
+		printf("two");
+		break;
+		}
+		case 3:
+		{
+             	printf("three");
+		break;
+		}
+		default:
+		{
+		printf("hii");
+		}
+		return 0;
+	}
+		}
